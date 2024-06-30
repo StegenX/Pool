d24ad8f2f98e94acda219cfd84a20342d21db7da
Stegen <152444769+StegenX@users.noreply.github.com>
2024-06-30 21:28:04 +0100

    Add files via upload

---
+++ c01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aagharbi <aagharbi@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/30 18:51:57 by aagharbi          #+#    #+#             */
+/*   Updated: 2024/06/30 20:08:33 by aagharbi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(int *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int		i;
+	int		temp ;
+
+	i = 0;
+	while (i < (size / 2))
+	{
+		temp = tab[i];
+		tab[i] = tab[size - i - 1];
+		tab[size - i - 1] = temp;
+		i++;
+	}
+}
