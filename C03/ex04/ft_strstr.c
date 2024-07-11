80445f2779bcbf076448eb09413494f18790d0c4
Stegen <152444769+StegenX@users.noreply.github.com>
2024-07-11 13:42:24 +0100

    Add files via upload

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,8 +6,31 @@
 /*   By: aagharbi <aagharbi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 13:40:27 by aagharbi          #+#    #+#             */
-/*   Updated: 2024/07/07 16:01:35 by aagharbi         ###   ########.fr       */
+/*   Updated: 2024/07/11 13:37:57 by aagharbi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
 
+	i = 0;
+	if (to_find[0] == '\0')
+		return (str);
+	while (str[i] != '\0')
+	{
+		if (str[i] == to_find[0])
+		{
+			j = 0;
+			while (to_find[j] != '\0' && str[i + j] == to_find[j])
+			{
+				j++;
+			}
+			if (to_find[j] == '\0')
+				return (str + i);
+		}
+		i++;
+	}
+	return (0);
+}
