c88f67ad751df5cecaeac9d6c0d14a67de4484d0
Stegen <152444769+StegenX@users.noreply.github.com>
2024-07-15 15:34:47 +0100

    Add files via upload

--- C05/ex02/ft_iterative_power.c
+++ C05/ex02/ft_iterative_power.c
@@ -6,7 +6,7 @@
 /*   By: aagharbi <aagharbi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 15:41:52 by aagharbi          #+#    #+#             */
-/*   Updated: 2024/07/11 15:44:05 by aagharbi         ###   ########.fr       */
+/*   Updated: 2024/07/15 15:34:29 by aagharbi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 int	ft_iterative_power(int nb, int power)
@@ -16,6 +16,10 @@ int	ft_iterative_power(int nb, int power)
 
 	i = 1;
 	result = 1;
+	if (power < 0)
+		return (0);
+	if (nb == 0 && power == 0)
+		return (1);
 	while (i <= power)
 	{
 		result *= nb;
