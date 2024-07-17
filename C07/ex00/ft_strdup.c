5916c92df154cd7baa568826277ed60984855520
Stegen <152444769+StegenX@users.noreply.github.com>
2024-07-17 17:41:19 +0100

    Add files via upload

--- C07/ex00/ft_strdup.c
+++ C07/ex00/ft_strdup.c
@@ -6,46 +6,29 @@
 /*   By: aagharbi <aagharbi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 17:23:10 by aagharbi          #+#    #+#             */
-/*   Updated: 2024/07/15 17:34:18 by aagharbi         ###   ########.fr       */
+/*   Updated: 2024/07/17 16:18:31 by aagharbi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
 
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		i++;
-	}
-	return (i);
-}
-
-char	*ft_strcpy(char *s1, char *s2)
-{
-	int	i;
-
-	i = 0;
-	while (s2[i])
-	{
-		s1[i] = s2[i];
-		i++;
-	}
-	s1[i] = '\0';
-	return (s1);
-}
-
 char	*ft_strdup(char *src)
 {
 	char	*dest;
-	int		len;
+	int		i;
 
-	len = ft_strlen(src);
-	dest = malloc(len * sizeof(char) + 1);
+	while (src[i])
+	{
+		i++;
+	}
+	dest = malloc(i * sizeof(char) + 1);
 	if (dest == NULL)
 		return (NULL);
-	ft_strcpy(dest, src);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -6,39 +6,76 @@
 /*   By: aagharbi <aagharbi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 18:43:14 by aagharbi          #+#    #+#             */
-/*   Updated: 2024/07/16 18:44:23 by aagharbi         ###   ########.fr       */
+/*   Updated: 2024/07/17 17:40:44 by aagharbi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
 
-char	*ft_strjoin(int size, char **strs, char *sep)
+int	ft_strlen(char *str)
 {
-	char	*str;
-	int		i;
-	int		j;
-	int		c;
+	int	i;
 
-	str = malloc(sizeof(char));
 	i = 0;
-	c = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	ft_total_size(int size, char **strs, char *sep)
+{
+	int	i;
+	int	sep_len;
+	int	total_len;
+
+	i = 0;
+	sep_len = ft_strlen(sep);
+	total_len = 0;
 	while (i < size)
 	{
-		j = 0;
-		while (strs[i][j] != '\0')
-		{
-			str[c] = strs[i][j];
-			c++;
-			j++;
-		}
-		j = 0;
-		while (sep[j] != '\0' && i != size - 1)
-		{
-			str[c] = sep[j];
-			c++;
-			j++;
-		}
+		total_len += ft_strlen(strs[i]);
+		if (i < size - 1)
+			total_len += sep_len;
 		i++;
 	}
-	str[c] = '\0';
-	return (str);
+	return (total_len);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*text;
+	int		i;
+	int		total_len;
+
+	i = 0;
+	total_len = ft_total_size(size, strs, sep) + 1;
+	text = malloc(sizeof(char) * total_len);
+	if (!text)
+		return (0);
+	*text = 0;
+	while (i < size)
+	{
+		ft_strcat(text, strs[i]);
+		if (i < size - 1)
+			ft_strcat(text, sep);
+		i++;
+	}
+	return (text);
 }
